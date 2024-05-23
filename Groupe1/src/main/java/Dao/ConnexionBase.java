4efec84eb7888a1cd46f0821e3ea33c04d35a89e
ndeyetah <158485250+ndeyetah@users.noreply.github.com>
2024-05-23 13:42:22 +0000

    Add files via upload

---
+++ Groupe1/src/main/java/Dao/ConnexionBase.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Dao;
+
+/**
+ *
+ * @author DESKLAPRO
+ */
+public class ConnexionBase {
+    public void conBD(){
+        
+    }
+}
